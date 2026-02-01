463106581eff249f2a4a0be6f6fc96a68fbd31b6
DnBright <dnbright000@gmail.com>
2026-02-01 09:57:22 +0700

    chore: add HTML debug banner v2.3

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div style="background: red; color: white; padding: 10px; text-align: center; font-weight: bold; font-family: sans-serif;">⚠️ SERVER CONNECTED v2.3 - IF YOU SEE THIS, PHP IS UPDATED ⚠️</div>
 <div id="admin-dashboard-root" data-stats="{{ json_encode([
     'totalLeads' => $totalLeads,
     'newLeads' => $newLeads,
